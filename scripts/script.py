72d38b8d2bd39257ab5802762d655dc94554611d
keyshor <kishor1996@gmail.com>
2021-05-13 19:40:59 -0400

    changed constant rewards for fetch

--- scripts/script.py
+++ scripts/script.py
@@ -3,7 +3,7 @@ import os
 env = 'Fetch'
 alg = "dhrm"
 
-mydict = {}
+mydict: dict = {}
 mydict['Rooms9'] = {}
 mydict['Rooms9']['time'] = '3e6'
 mydict['Rooms9']['rm'] = [('M1', '1.6e6'), ('M2', '3e6'), ('M4', '2e6')]
@@ -12,7 +12,8 @@ mydict['Rooms9']['options'] = ['rs', 'rc', 'cr']
 
 mydict['Rooms9C'] = {}
 mydict['Rooms9C']['time'] = '3e6'
-mydict['Rooms9C']['rm'] = [('M1', '2e6'), ('M2', '2e6'), ('M3', '3.2e6'), ('M4', '1.5e6'),('M5', '3.2e6')]
+mydict['Rooms9C']['rm'] = [('M1', '2e6'), ('M2', '2e6'), ('M3', '3.2e6'),
+                           ('M4', '1.5e6'), ('M5', '3.2e6')]
 mydict['Rooms9C']['gamma'] = 0.99
 mydict['Rooms9C']['options'] = ['cr', 'basic']
 
@@ -24,24 +25,24 @@ mydict['Rooms16']['options'] = ['rs', 'cr', 'rc']
 
 mydict['Rooms16C'] = {}
 mydict['Rooms16C']['time'] = '1.5e7'
-#mydict['Rooms16C']['rm'] = [('M1', '3.3e6'), ('M2', '5.2e6'), ('M3', '9e6'), ('M4', '1.7e7'), ('M5', '2.1e7')]
+# mydict['Rooms16C']['rm'] = [('M1', '3.3e6'), ('M2', '5.2e6'),
+# ('M3', '9e6'), ('M4', '1.7e7'), ('M5', '2.1e7')]
 mydict['Rooms16C']['rm'] = [('M3', '9e6'), ('M4', '1.7e7'), ('M5', '2.1e7')]
 mydict['Rooms16C']['gamma'] = 0.99
 mydict['Rooms16C']['options'] = ['cr', 'basic']
 
 mydict['Fetch'] = {}
 mydict['Fetch']['time'] = '2.5e5'
-mydict['Fetch']['rm'] = [('M1', '2.2e5'), ('M2','2.2e5'), ('M3','4.2e5')]
+mydict['Fetch']['rm'] = [('M1', '2.2e5'), ('M2', '2.2e5'), ('M3', '4.2e5')]
 mydict['Fetch']['gamma'] = 0.95
 mydict['Fetch']['options'] = ['cr', 'basic']
 
 
-
-num_iter = 3
+num_iter = 5
 DIR = '../results'
 dict1 = mydict[env]
-for (rm,timestep) in dict1['rm']:
-    env_name = '-'.join([env, rm,'v0'])
+for (rm, timestep) in dict1['rm']:
+    env_name = '-'.join([env, rm, 'v0'])
     for option in dict1['options']:
         if option == 'rs':
             flag = '--use_rs'
@@ -52,21 +53,21 @@ for (rm,timestep) in dict1['rm']:
         if option == "basic":
             flag = ''
 
-        if alg=="dhrm":
+        if alg == "dhrm":
             flag += " --r_max=1000 --r_mix=-1000 "
-        dirname = '/'.join([DIR,env,rm,alg, option])
+        dirname = '/'.join([DIR, env, rm, alg, option])
         if not os.path.exists(dirname):
             os.makedirs(dirname)
 
         for i in range(num_iter):
-            path = dirname + "/" + str(i) 
-            #print(path)
+            path = dirname + "/" + str(i)
+            # print(path)
 
             temp = env+rm+alg+option+str(i)
-            
+
             cmd = " ".join(['screen -dmS', temp, 'python3 run.py --alg='+alg,
-                           '--env='+env_name,
-                           '--num_timesteps='+timestep,
+                            '--env='+env_name,
+                            '--num_timesteps='+timestep,
                             '--gamma='+str(dict1['gamma']),
                             "--log_path="+path,
                             flag])
